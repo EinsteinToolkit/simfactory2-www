395736ad56ae08bf284e812ef2aed755d3d1f4bb
hinder <hinder@cde0f5ca-dead-0410-9a94-d5ce4c5b1455>
2018-08-02 22:37:07 +0000

    Change user guide link to point to simfactory.bitbucket.io
    git-svn-id: https://svn.cct.lsu.edu/repos/numrel/simfactory2/www@2445 cde0f5ca-dead-0410-9a94-d5ce4c5b1455

--- info/documentation/index.php
+++ info/documentation/index.php
@@ -4,7 +4,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/global/header.php');?>
 <p> The Simulation Factory 2.0, Python
 
 <ul>
-<li><a href='userguide'><b>User guide</b></a> - work in progress
+<li><a href='https://simfactory.bitbucket.io/simfactory2/userguide/'><b>User guide</b></a> - work in progress
 <li><a href='http://docs.einsteintoolkit.org/et-docs/Pysim_Tutorial_for_New_Users'><b>Tutorial For New Users</b></a> - outdated
 <li><a href='http://docs.einsteintoolkit.org/et-docs/Simulation_Factory_Advanced_Tutorial'><b>Advanced Tutorial</b></a> - outdated
 <li><a href='http://arxiv.org/abs/1008.4571'><b>Simulation Factory: Taming Application Configuration and Workflow on High-End Resources</b></a>
