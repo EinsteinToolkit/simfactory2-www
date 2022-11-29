749633a98a15911b31f8d649b67ec62f8c6a969b
Roland Haas <rhaas@illinois.edu>
2022-11-29 09:43:24 -0600

    New release.

--- simfactory/download/index.php
+++ simfactory/download/index.php
@@ -11,7 +11,7 @@ have it in the directory Cactus/simfactory.</p>
 
 <p>Stable:</p>
 
-<pre>git clone -b ET_2022_05 https://bitbucket.org/simfactory/simfactory2.git</pre>
+<pre>git clone -b ET_2022_11 https://bitbucket.org/simfactory/simfactory2.git</pre>
 
 <p>Development:</p>
 
